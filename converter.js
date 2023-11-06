5f773b45b416a5ef09fec23a942d44c9b6b47269
zoeperezc <110468532+zoeperezc@users.noreply.github.com>
2023-11-06 12:13:46 -0300

    refactor(converter): platos

--- converter.js
+++ converter.js
@@ -1,4 +1,4 @@
-const plates = require('./menu');
+const platos = require('./menu');
 const mysql2 = require('mysql2');
 
 const connection = mysql2.createConnection({
@@ -13,8 +13,8 @@ connection.connect((err) => {
     console.log('Connected!');
 });
 
-plates.forEach(element => {
-    connection.query("INSERT INTO plates (id, nombre, precio, descripcion, tipo) VALUES (?, ?, ?, ?, ?);", [element.id, element.nombre, element.precio, element.descripcion, element.tipo], (err, rows) => {
+platos.forEach(element => {
+    connection.query("INSERT INTO platos (id, nombre, precio, descripcion, tipo) VALUES (?, ?, ?, ?, ?);", [element.id, element.nombre, element.precio, element.descripcion, element.tipo], (err, rows) => {
         console.log(err);
         console.log(rows);
     });
